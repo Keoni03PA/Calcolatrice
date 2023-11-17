287d68e2d2f3f0ff9c064579aa8de298abe57d1b
Keoni03PA <palumbo.g.manuel@gmail.com>
2023-11-17 14:07:27 +0100

    debug

--- Calcolatrice/Calcolatrice.java
+++ Calcolatrice/Calcolatrice.java
@@ -2,7 +2,7 @@ package Calcolatrice;
 
 import java.util.Scanner;
 
-public class develop {
+public class Calcolatrice{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
